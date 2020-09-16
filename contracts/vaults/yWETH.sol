EdsonAlcala/contracts

Vault[WETH]: 2020-09-01
yWETH - https://etherscan.io/address/0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7#code

contracts/vaults/yWETH.sol
@@ -1,0 +1,171 @@
+pragma solidity ^0.5.16;
+
+import "@openzeppelinV2/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV2/contracts/math/SafeMath.sol";
+import "@openzeppelinV2/contracts/utils/Address.sol";
+import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelinV2/contracts/ownership/Ownable.sol";
+
+import "../../interfaces/weth/WETH.sol";
+import "../../interfaces/yearn/Controller.sol";
+
+// NOTE: The name of this contract was modified from yVault so as not to conflict with yVault.sol
+contract yWETH is ERC20, ERC20Detailed {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    IERC20 public token;
+
+    uint public min = 9990;
+    uint public constant max = 10000;
+
+    address public governance;
+    address public controller;
+
+    constructor (address _token, address _controller) public ERC20Detailed(
+        string(abi.encodePacked("yearn ", ERC20Detailed(_token).name())),
+        string(abi.encodePacked("y", ERC20Detailed(_token).symbol())),
+        ERC20Detailed(_token).decimals()
+    ) {
+        token = IERC20(_token);
+        governance = msg.sender;
+        controller = _controller;
+    }
+
+    function balance() public view returns (uint) {
+        return token.balanceOf(address(this))
+                .add(Controller(controller).balanceOf(address(token)));
+    }
+
+    function setMin(uint _min) external {
+        require(msg.sender == governance, "!governance");
+        min = _min;
+    }
+
+    function setGovernance(address _governance) public {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+    }
+
+    function setController(address _controller) public {
+        require(msg.sender == governance, "!governance");
+        controller = _controller;
+    }
+
+    // Custom logic in here for how much the vault allows to be borrowed
+    // Sets minimum required on-hand to keep small withdrawals cheap
+    function available() public view returns (uint) {
+        return token.balanceOf(address(this)).mul(min).div(max);
+    }
+
+    function earn() public {
+        uint _bal = available();
+        token.safeTransfer(controller, _bal);
+        Controller(controller).earn(address(token), _bal);
+    }
+
+    function depositAll() external {
+        deposit(token.balanceOf(msg.sender));
+    }
+
+    function deposit(uint _amount) public {
+        uint _pool = balance();
+        uint _before = token.balanceOf(address(this));
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+        uint _after = token.balanceOf(address(this));
+        _amount = _after.sub(_before); // Additional check for deflationary tokens
+        uint shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function depositETH() public payable {
+        uint _pool = balance();
+        uint _before = token.balanceOf(address(this));
+        uint _amount = msg.value;
+        WETH(address(token)).deposit.value(_amount)();
+        uint _after = token.balanceOf(address(this));
+        _amount = _after.sub(_before); // Additional check for deflationary tokens
+        uint shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawAll() external {
+        withdraw(balanceOf(msg.sender));
+    }
+
+    function withdrawAllETH() external {
+        withdrawETH(balanceOf(msg.sender));
+    }
+
+
+    // Used to swap any borrowed reserve over the debt limit to liquidate to 'token'
+    function harvest(address reserve, uint amount) external {
+        require(msg.sender == controller, "!controller");
+        require(reserve != address(token), "token");
+        IERC20(reserve).safeTransfer(controller, amount);
+    }
+
+    // No rebalance implementation for lower fees and faster swaps
+    function withdraw(uint _shares) public {
+        uint r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Check balance
+        uint b = token.balanceOf(address(this));
+        if (b < r) {
+            uint _withdraw = r.sub(b);
+            Controller(controller).withdraw(address(token), _withdraw);
+            uint _after = token.balanceOf(address(this));
+            uint _diff = _after.sub(b);
+            if (_diff < _withdraw) {
+                r = b.add(_diff);
+            }
+        }
+
+        token.safeTransfer(msg.sender, r);
+    }
+
+    // No rebalance implementation for lower fees and faster swaps
+    function withdrawETH(uint _shares) public {
+        uint r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Check balance
+        uint b = token.balanceOf(address(this));
+        if (b < r) {
+            uint _withdraw = r.sub(b);
+            Controller(controller).withdraw(address(token), _withdraw);
+            uint _after = token.balanceOf(address(this));
+            uint _diff = _after.sub(b);
+            if (_diff < _withdraw) {
+                r = b.add(_diff);
+            }
+        }
+
+        WETH(address(token)).withdraw(r);
+        address(msg.sender).transfer(r);
+    }
+
+    function getPricePerFullShare() public view returns (uint) {
+        return balance().mul(1e18).div(totalSupply());
+    }
+
+    function () external payable {
+        if (msg.sender != address(token)) {
+            depositETH();
+        }
+    }
+}

endlessor/yearn-protocol

contracts/vaults/yWETH.sol
@@ -1,0 +1,171 @@
+pragma solidity ^0.5.16;
+
+import "@openzeppelinV2/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV2/contracts/math/SafeMath.sol";
+import "@openzeppelinV2/contracts/utils/Address.sol";
+import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelinV2/contracts/ownership/Ownable.sol";
+
+import "../../interfaces/weth/WETH.sol";
+import "../../interfaces/yearn/IController.sol";
+
+// NOTE: The name of this contract was modified from yVault so as not to conflict with yVault.sol
+contract yWETH is ERC20, ERC20Detailed {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    IERC20 public token;
+
+    uint public min = 9990;
+    uint public constant max = 10000;
+
+    address public governance;
+    address public controller;
+
+    constructor (address _token, address _controller) public ERC20Detailed(
+        string(abi.encodePacked("yearn ", ERC20Detailed(_token).name())),
+        string(abi.encodePacked("y", ERC20Detailed(_token).symbol())),
+        ERC20Detailed(_token).decimals()
+    ) {
+        token = IERC20(_token);
+        governance = msg.sender;
+        controller = _controller;
+    }
+
+    function balance() public view returns (uint) {
+        return token.balanceOf(address(this))
+                .add(IController(controller).balanceOf(address(token)));
+    }
+
+    function setMin(uint _min) external {
+        require(msg.sender == governance, "!governance");
+        min = _min;
+    }
+
+    function setGovernance(address _governance) public {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+    }
+
+    function setController(address _controller) public {
+        require(msg.sender == governance, "!governance");
+        controller = _controller;
+    }
+
+    // Custom logic in here for how much the vault allows to be borrowed
+    // Sets minimum required on-hand to keep small withdrawals cheap
+    function available() public view returns (uint) {
+        return token.balanceOf(address(this)).mul(min).div(max);
+    }
+
+    function earn() public {
+        uint _bal = available();
+        token.safeTransfer(controller, _bal);
+        IController(controller).earn(address(token), _bal);
+    }
+
+    function depositAll() external {
+        deposit(token.balanceOf(msg.sender));
+    }
+
+    function deposit(uint _amount) public {
+        uint _pool = balance();
+        uint _before = token.balanceOf(address(this));
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+        uint _after = token.balanceOf(address(this));
+        _amount = _after.sub(_before); // Additional check for deflationary tokens
+        uint shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function depositETH() public payable {
+        uint _pool = balance();
+        uint _before = token.balanceOf(address(this));
+        uint _amount = msg.value;
+        WETH(address(token)).deposit.value(_amount)();
+        uint _after = token.balanceOf(address(this));
+        _amount = _after.sub(_before); // Additional check for deflationary tokens
+        uint shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawAll() external {
+        withdraw(balanceOf(msg.sender));
+    }
+
+    function withdrawAllETH() external {
+        withdrawETH(balanceOf(msg.sender));
+    }
+
+
+    // Used to swap any borrowed reserve over the debt limit to liquidate to 'token'
+    function harvest(address reserve, uint amount) external {
+        require(msg.sender == controller, "!controller");
+        require(reserve != address(token), "token");
+        IERC20(reserve).safeTransfer(controller, amount);
+    }
+
+    // No rebalance implementation for lower fees and faster swaps
+    function withdraw(uint _shares) public {
+        uint r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Check balance
+        uint b = token.balanceOf(address(this));
+        if (b < r) {
+            uint _withdraw = r.sub(b);
+            IController(controller).withdraw(address(token), _withdraw);
+            uint _after = token.balanceOf(address(this));
+            uint _diff = _after.sub(b);
+            if (_diff < _withdraw) {
+                r = b.add(_diff);
+            }
+        }
+
+        token.safeTransfer(msg.sender, r);
+    }
+
+    // No rebalance implementation for lower fees and faster swaps
+    function withdrawETH(uint _shares) public {
+        uint r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Check balance
+        uint b = token.balanceOf(address(this));
+        if (b < r) {
+            uint _withdraw = r.sub(b);
+            IController(controller).withdraw(address(token), _withdraw);
+            uint _after = token.balanceOf(address(this));
+            uint _diff = _after.sub(b);
+            if (_diff < _withdraw) {
+                r = b.add(_diff);
+            }
+        }
+
+        WETH(address(token)).withdraw(r);
+        address(msg.sender).transfer(r);
+    }
+
+    function getPricePerFullShare() public view returns (uint) {
+        return balance().mul(1e18).div(totalSupply());
+    }
+
+    function () external payable {
+        if (msg.sender != address(token)) {
+            depositETH();
+        }
+    }
+}